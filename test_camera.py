d8f42e5284393a21923d80bace18a79d7b3ad009
mrsrv77 <sarveshsingh54362@gmail.com>
2026-07-08 15:46:04 +0530

    Initial local project

---
+++ test_camera.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Camera Not Working")
+        break
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
